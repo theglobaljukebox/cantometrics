11898b28c74e4121b439bdaf6f758d3fbd837b8f
Sam Passmore <passmore.sam@gmail.com>
2021-02-15 14:06:26 +1300

    remove column numbers from data.csv

--- to_long.py
+++ to_long.py
@@ -3,4 +3,4 @@ from pandas import read_csv, melt
 
 data = read_csv("raw/data.csv")
 long_df = melt(data, id_vars = ['canto_coding_id', 'Culture', 'C_cid'], var_name='var_id', value_name = 'code')
-long_df.to_csv("cldf/data.csv")
+long_df.to_csv("cldf/data.csv", index=False)
