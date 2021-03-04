a2521cea32b40b31a92861054ffa9daa7cf1f0c9
Sam Passmore <passmore.sam@gmail.com>
2021-03-04 13:52:14 +1300

    add tests for lines from John McBride

---
+++ test_cantometrics.py
@@ -0,0 +1,32 @@
+import pytest
+import pandas as pd
+import datatest as dt 
+
+
+@pytest.fixture(scope='module')
+@dt.working_directory(__file__)
+def df():
+    return pd.read_csv('raw/data.csv')
+
+## Testing that all codes are valid
+
+# Test for single value
+def is_coding_allowed(integer, n=13):
+    for i in range(n,0,-1):
+        if integer == 0:
+            return True
+        if integer >= 2**i:
+            integer -= 2**i
+    return True if integer == 0 else False
+
+def test_column(df):
+    for col in df.columns:
+        if 'cv_' in col:
+            print(col)
+            assert all([is_coding_allowed(x) == True for x in df[col]])
+
+
+
+# def test_runtime(df):
+#     dt.validate(df['canto_coding_id'], int)
+
